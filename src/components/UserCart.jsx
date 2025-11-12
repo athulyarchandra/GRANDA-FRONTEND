4207d517a62283d284c29e0633e25e59e22ddeb6
athulyarchandra <athulyarchandra24@gmail.com>
2025-11-12 11:19:52 +0530

    serverurl

--- src/components/UserCart.jsx
+++ src/components/UserCart.jsx
@@ -157,7 +157,7 @@ const UserCart = () => {
                     <tr className="text-center " key={index}>
                       <td className="px-2 py-2 text-left align-top">
                         <img
-                          src={`http://13.60.49.139:3000/uploads/${item.images[0]}`}
+                          src={item.productImages?.[0]}
                           alt="Product"
                           className="w-[70px] sm:w-[100px] h-[70px] sm:h-[100px] mr-2 inline-block rounded object-contain"
                         />
