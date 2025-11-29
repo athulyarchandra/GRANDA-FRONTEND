068ad52e23257298d48980dc0ea27d164b191b14
athulyarchandra <athulyarchandra24@gmail.com>
2025-11-29 21:50:40 +0530

    displaying cart image

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -16,12 +16,12 @@ const Clients = () => {
   </div>
 
   <div className="w-full max-w-7xl grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-    <div className="relative text-center transition-transform duration-300 p-6 bg-white rounded-[30px] outline-1 outline-offset-[-1px] outline-[#f5f7f9] flex flex-col justify-center items-center gap-5 hover:scale-105 hover:rotate-[4deg] shadow-[0px_25px_52px_-12px_rgba(17,17,17,0.20)]">
+    <div className="relative text-center transition-transform duration-100 p-6 bg-white rounded-[30px] outline-1 outline-offset-[-1px] outline-[#f5f7f9] flex flex-col justify-center items-center gap-5 hover:scale-105 hover:rotate-[1deg] shadow-[0px_25px_52px_-12px_rgba(17,17,17,0.20)]">
       <img src="https://i.ibb.co/39QLHv5H/1693926367335.jpg" alt="Hugo Pakula" className="w-16 h-16 rounded-full object-cover border-4 border-white shadow-md" />
       <div className="space-y-6">
         <h4 className="text-[#323b47] text-xl font-bold leading-tight">Hugo Pakula</h4>
         <p className="text-[#58677d] text-lg font-medium leading-snug">
-          Working with Ab Nahid Agency was a game-changer. Their web development and SEO support significantly boosted my site's visibility and performance.
+         Fast delivery, amazing quality, and excellent customer service! I’ll definitely shop here again.
         </p>
       </div>
       <div className="p-3.5 rounded-full outline-1 outline-offset-[-1px] outline-[#e9eff7] flex justify-center items-center">
@@ -30,12 +30,12 @@ const Clients = () => {
     </div>
 
 
-    <div className="relative text-center transition-transform duration-300 p-6 bg-white rounded-[30px] outline-1 outline-offset-[-1px] outline-[#f5f7f9] flex flex-col justify-center items-center gap-5 hover:scale-105 hover:rotate-[4deg] shadow-[0px_25px_52px_-12px_rgba(17,17,17,0.20)]">
+    <div className="relative text-center transition-transform duration-300 p-6 bg-white rounded-[30px] outline-1 outline-offset-[-1px] outline-[#f5f7f9] flex flex-col justify-center items-center gap-5 hover:scale-105 hover:rotate-[1deg] shadow-[0px_25px_52px_-12px_rgba(17,17,17,0.20)]">
       <img src="https://i.ibb.co/Zz7HrXgJ/1750602389688-e-1756944000-v-beta-t-rsy-A7-Ftr-NJaz-CX1-VS9lkuh12-Hon-WNhh-Kes1n4wd6k-SA.jpg" alt="Ajmaine Bhuiyan" className="w-16 h-16 rounded-full object-cover border-4 border-white shadow-md" />
       <div className="space-y-6">
         <h4 className="text-[#323b47] text-xl font-bold leading-tight">Ajmaine Bhuiyan</h4>
         <p className="text-[#58677d] text-lg font-medium leading-snug">
-          They delivered exactly what I needed — a modern, fast, and SEO-optimized website for my startup. Highly recommended for entrepreneurs.
+         I was blown away by the quality of the products. Everything looks exactly like the pictures, and the packaging was perfect. Highly recommend!
         </p>
       </div>
       <div className="p-3.5 rounded-full outline-1 outline-offset-[-1px] outline-[#e9eff7] flex justify-center items-center">
@@ -43,12 +43,12 @@ const Clients = () => {
       </div>
     </div>
 
-    <div className="relative text-center transition-transform duration-300 p-6 bg-white rounded-[30px] outline-1 outline-offset-[-1px] outline-[#f5f7f9] flex flex-col justify-center items-center gap-5 hover:scale-105 hover:rotate-[4deg] shadow-[0px_25px_52px_-12px_rgba(17,17,17,0.20)]">
-      <img src="https://i.ibb.co/HLCKTh8F/1742201429729.jpg" alt="Josh Gardner" className="w-16 h-16 rounded-full object-cover border-4 border-white shadow-md" />
+    <div className="relative text-center transition-transform duration-300 p-6 bg-white rounded-[30px] outline-1 outline-offset-[-1px] outline-[#f5f7f9] flex flex-col justify-center items-center gap-5 hover:scale-105 hover:rotate-[1deg] shadow-[0px_25px_52px_-12px_rgba(17,17,17,0.20)]">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZkXf0xG9CVHgCmcViTjkc8XnnSH4pdGTQlAVAg3gSzvUsOkdxQS77IhTW5Zsz8CFZ514&usqp=CAU" alt="Josh Gardner" className="w-16 h-16 rounded-full object-cover border-4 border-white shadow-md" />
       <div className="space-y-6">
-        <h4 className="text-[#323b47] text-xl font-bold leading-tight">Josh Gardner</h4>
+        <h4 className="text-[#323b47] text-xl font-bold leading-tight">Josh </h4>
         <p className="text-[#58677d] text-lg font-medium leading-snug">
-          Their UI/UX design skills are impressive. My site now looks professional and loads super fast across all devices. Excellent communication too!
+         The support team was so helpful when I had questions about my order. They went above and beyond to make sure I was happy with my purchase!
         </p>
       </div>
       <div className="p-3.5 rounded-full outline-1 outline-offset-[-1px] outline-[#e9eff7] flex justify-center items-center">
@@ -56,24 +56,24 @@ const Clients = () => {
       </div>
     </div>
 
-    <div className="relative text-center transition-transform duration-300 p-6 bg-white rounded-[30px] outline-1 outline-offset-[-1px] outline-[#f5f7f9] flex flex-col justify-center items-center gap-5 hover:scale-105 hover:rotate-[4deg] shadow-[0px_25px_52px_-12px_rgba(17,17,17,0.20)]">
-      <img src="https://i.ibb.co/39QLHv5H/1693926367335.jpg" alt="Hugo Pakula" className="w-16 h-16 rounded-full object-cover border-4 border-white shadow-md" />
+    <div className="relative text-center transition-transform duration-300 p-6 bg-white rounded-[30px] outline-1 outline-offset-[-1px] outline-[#f5f7f9] flex flex-col justify-center items-center gap-5 hover:scale-105 hover:rotate-[1deg] shadow-[0px_25px_52px_-12px_rgba(17,17,17,0.20)]">
+      <img src="https://thispersonnotexist.org/static/img/Random_female_face_1.jpeg" alt="Hugo Pakula" className="w-16 h-16 rounded-full object-cover border-4 border-white shadow-md" />
       <div className="space-y-6">
-        <h4 className="text-[#323b47] text-xl font-bold leading-tight">Hugo Pakula</h4>
+        <h4 className="text-[#323b47] text-xl font-bold leading-tight">Priya S</h4>
         <p className="text-[#58677d] text-lg font-medium leading-snug">
-          Working with Ab Nahid Agency was a game-changer. Their web development and SEO support significantly boosted my site's visibility and performance.
+         I’ve been shopping here for months and have never been disappointed. Great products, fast shipping, and always reliable!
         </p>
       </div>
       <div className="p-3.5 rounded-full outline-1 outline-offset-[-1px] outline-[#e9eff7] flex justify-center items-center">
         <img src="https://img.icons8.com/?size=100&id=38968&format=png&color=000000" alt="quote" className="w-6 h-6" />
       </div>
     </div>
-    <div className="relative text-center transition-transform duration-300 p-6 bg-white rounded-[30px] outline-1 outline-offset-[-1px] outline-[#f5f7f9] flex flex-col justify-center items-center gap-5 hover:scale-105 hover:rotate-[4deg] shadow-[0px_25px_52px_-12px_rgba(17,17,17,0.20)]">
-      <img src="https://i.ibb.co/Zz7HrXgJ/1750602389688-e-1756944000-v-beta-t-rsy-A7-Ftr-NJaz-CX1-VS9lkuh12-Hon-WNhh-Kes1n4wd6k-SA.jpg" alt="Ajmaine Bhuiyan" className="w-16 h-16 rounded-full object-cover border-4 border-white shadow-md" />
+    <div className="relative text-center transition-transform duration-300 p-6 bg-white rounded-[30px] outline-1 outline-offset-[-1px] outline-[#f5f7f9] flex flex-col justify-center items-center gap-5 hover:scale-105 hover:rotate-[1deg] shadow-[0px_25px_52px_-12px_rgba(17,17,17,0.20)]">
+      <img src="https://images.generated.photos/ETvAVQ7nhh8-43EetE3eH6dMvjwD6w_Mm2biNL8L7zg/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MjYxNDI5LmpwZw.jpg" alt="Ajmaine Bhuiyan" className="w-16 h-16 rounded-full object-cover border-4 border-white shadow-md" />
       <div className="space-y-6">
-        <h4 className="text-[#323b47] text-xl font-bold leading-tight">Ajmaine Bhuiyan</h4>
+        <h4 className="text-[#323b47] text-xl font-bold leading-tight">Karan T</h4>
         <p className="text-[#58677d] text-lg font-medium leading-snug">
-          They delivered exactly what I needed — a modern, fast, and SEO-optimized website for my startup. Highly recommended for entrepreneurs.
+         Shopping on this website was super easy and stress-free. The checkout process is smooth, and I received my order in no time!
         </p>
       </div>
       <div className="p-3.5 rounded-full outline-1 outline-offset-[-1px] outline-[#e9eff7] flex justify-center items-center">
@@ -81,12 +81,12 @@ const Clients = () => {
       </div>
     </div>
 
-    <div className="relative text-center transition-transform duration-300 p-6 bg-white rounded-[30px] outline-1 outline-offset-[-1px] outline-[#f5f7f9] flex flex-col justify-center items-center gap-5 hover:scale-105 hover:rotate-[4deg] shadow-[0px_25px_52px_-12px_rgba(17,17,17,0.20)]">
-      <img src="https://i.ibb.co/HLCKTh8F/1742201429729.jpg" alt="Josh Gardner" className="w-16 h-16 rounded-full object-cover border-4 border-white shadow-md" />
+    <div className="relative text-center transition-transform duration-300 p-6 bg-white rounded-[30px] outline-1 outline-offset-[-1px] outline-[#f5f7f9] flex flex-col justify-center items-center gap-5 hover:scale-105 hover:rotate-[1deg] shadow-[0px_25px_52px_-12px_rgba(17,17,17,0.20)]">
+      <img src="https://images.generated.photos/SuUv2tQSzHjOYuULXCPF9BcPW1z-zAqR5zf1r44gZgE/rs:fit:256:256/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/MTg3NDU2LmpwZw.jpg" alt="Josh Gardner" className="w-16 h-16 rounded-full object-cover border-4 border-white shadow-md" />
       <div className="space-y-6">
-        <h4 className="text-[#323b47] text-xl font-bold leading-tight">Josh Gardner</h4>
+        <h4 className="text-[#323b47] text-xl font-bold leading-tight">Aisha Gardner</h4>
         <p className="text-[#58677d] text-lg font-medium leading-snug">
-          Their UI/UX design skills are impressive. My site now looks professional and loads super fast across all devices. Excellent communication too!
+          I didn’t think online shopping could be this enjoyable! My package arrived quickly, and I love everything I ordered. 10/10!
         </p>
       </div>
       <div className="p-3.5 rounded-full outline-1 outline-offset-[-1px] outline-[#e9eff7] flex justify-center items-center">
--- src/components/UserCart.jsx
+++ src/components/UserCart.jsx
@@ -159,7 +159,7 @@ const UserCart = () => {
                     <tr className="text-center " key={index}>
                       <td className="px-2 py-2 text-left align-top">
                         <img
-                          src={item.productImages?.[0]}
+                          src={item.images?.[0]}
                           alt="Product"
                           className="w-[70px] sm:w-[100px] h-[70px] sm:h-[100px] mr-2 inline-block rounded object-contain"
                         />
